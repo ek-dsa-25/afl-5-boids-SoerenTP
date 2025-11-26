c7e9e3ff81b27dce2779a2ebfe568148816939e5
SoerenTP <144678528+SoerenTP@users.noreply.github.com>
2025-11-26 13:49:15 +0100

    Update BoidType.java

--- src/main/model/BoidType.java
+++ src/main/model/BoidType.java
@@ -1,9 +1,15 @@
 package main.model;
 
+import main.behavior.BehaviorStrategy;
+import main.behavior.FlockBehavior;
+import main.behavior.FlockWeights;
+import main.behavior.RandomBehavior;
+
 import java.awt.Color;
 
 public enum BoidType {
-    STANDARD(Color.WHITE);
+    STANDARD(Color.WHITE),
+    RANDOM_WALKER(Color.GREEN);
 
     private final Color color;
 
@@ -13,5 +19,13 @@ public enum BoidType {
 
     public Color getColor() {
         return color;
+    }
+    
+    public static BehaviorStrategy getStrategyForType(BoidType type) {
+        if (type == RANDOM_WALKER) {
+            return new RandomBehavior();
+        }
+        
+        return new FlockBehavior(FlockWeights.standard());
     }
 }
